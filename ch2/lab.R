2275bd25f3f8a147c8fe77a18717191700877e78
Rachel-lxy <rachel.lixinyu@gmail.com>
2017-01-04 16:02:06 -0500

    ch2 lab

--- ch2/lab.R
+++ ch2/lab.R
@@ -1,6 +1,7 @@
 # Chapter 2 Lab: Introduction to R
 
 # 2.3.1 Basic Commands
+
 x <- c(1,3,2,5)
 x
 x = c(1,6,2)
@@ -34,6 +35,7 @@ sqrt(var(y))
 sd(y)
 
 # 2.3.2 Graphics
+
 x = rnorm(100)
 y = rnorm(100)
 plot(x, y)
@@ -41,3 +43,69 @@ plot(x, y, xlab="this is the x-axis", ylab="this is the y-axis", main="Plot of X
 pdf("Figure.pdf")
 plot(x, y, col="green")
 dev.off()
+x = seq(1,10)
+x
+x = 1:10
+x
+x = seq(-pi, pi, length=50)
+y = x
+f = outer(x, y, function(x,y)cos(y)/(1+x^2))
+contour(x, y, f)
+contour(x, y, f, nlevels=45, add=T)
+fa = (f - t(f))/2
+contour(x, y, fa, nlevels=15)
+image(x, y, fa)
+persp(x, y, fa)
+persp(x, y, fa, theta=30)
+persp(x, y, fa, theta=30, phi=20)
+persp(x, y, fa, theta=30, phi=70)
+persp(x, y, fa, theta=30, phi=40)
+
+# 2.3.3 Indexing Data
+
+A = matrix(1:16, 4, 4)
+A
+A[2,3]
+A[c(1,3),c(2,4)]
+A[1:3,2:4]
+A[1:2,]
+A[,1:2]
+A[1,]
+A[-c(1,3),]
+A[-c(1,3),-c(1,3,4)]
+dim(A)
+
+# 2.3.4 Loading Data
+
+Auto = read.table("Auto.data")
+fix(Auto)
+Auto = read.table("Auto.data", header=T, na.strings="?")
+fix(Auto)
+Auto = read.csv("Auto.csv", header=T, na.strings="?")
+fix(Auto)
+dim(Auto)
+Auto[1:4,]
+Auto = na.omit(Auto)
+dim(Auto)
+names(Auto)
+
+# 2.3.5 Additional Graphical and Numerical Summaries
+
+plot(Auto$cylinders, Auto$mpg)
+attach(Auto)
+plot(cylinders, mpg)
+cylinders = as.factor(cylinders)
+plot(cylinders, mpg)
+plot(cylinders, mpg, col="red")
+plot(cylinders, mpg, col="red", varwidth=T)
+plot(cylinders, mpg, col="red", varwidth=T, horizontal=T)
+plot(cylinders, mpg, col="red", varwidth=T, xlab="cylinders", ylab="MPG")
+hist(mpg)
+hist(mpg, col=2)
+hist(mpg, col=2, breaks=15)
+pairs(Auto)
+pairs(~ mpg + displacement + horsepower + weight + acceleration, Auto)
+plot(horsepower, mpg)
+identify(horsepower, mpg, name)
+summary(Auto)
+summary(mpg)
